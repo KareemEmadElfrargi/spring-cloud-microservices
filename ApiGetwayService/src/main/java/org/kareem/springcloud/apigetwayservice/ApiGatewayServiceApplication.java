8eddbe8da528e179bb71e0e4cc59b3753cca4069
Kareem Emad <kareememad8561@gmail.com>
2026-05-04 00:53:04 +0300

    feat: Gateway in action

--- ApiGetwayService/src/main/java/org/kareem/springcloud/apigetwayservice/ApiGatewayServiceApplication.java
+++ ApiGetwayService/src/main/java/org/kareem/springcloud/apigetwayservice/ApiGatewayServiceApplication.java
@@ -2,12 +2,14 @@ package org.kareem.springcloud.apigetwayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class ApiGetwayServiceApplication {
+@EnableDiscoveryClient
+public class ApiGatewayServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiGetwayServiceApplication.class, args);
+        SpringApplication.run(ApiGatewayServiceApplication.class, args);
     }
 
 }
--- ApiGetwayService/src/test/java/org/kareem/springcloud/apigetwayservice/ApiGatewayServiceApplicationTests.java
+++ ApiGetwayService/src/test/java/org/kareem/springcloud/apigetwayservice/ApiGatewayServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiGetwayServiceApplicationTests {
+class ApiGatewayServiceApplicationTests {
 
     @Test
     void contextLoads() {
