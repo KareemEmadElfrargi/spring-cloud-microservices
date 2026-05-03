557a6e0d25e327301f46b68cb26a5e26facf8cc2
Kareem Emad <kareememad8561@gmail.com>
2026-05-04 00:07:19 +0300

    feat: Configure the routes

--- ProductService/src/main/java/org/kareem/springcloud/productservice/restclients/CouponClient.java
+++ ProductService/src/main/java/org/kareem/springcloud/productservice/restclients/CouponClient.java
@@ -5,10 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("coupon-service")
+@FeignClient("gateway-service")
 public interface CouponClient {
 
     @GetMapping("/couponapi/{code}")
     public Coupon getCoupon(@PathVariable String code);
-
 }
